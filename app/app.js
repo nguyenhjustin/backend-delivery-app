b8ffccdb1d1336769dcbc27969a28771c9579794
Justin Nguyen <nguyenhjustin@yahoo.com>
2018-07-12 22:19:14 -0700

    Fix formatting and add more comments

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const uuidv4 = require('uuid/v4');
 
+const express = require('express');
 const app = express();
 app.use(express.json());
 
@@ -19,15 +20,13 @@ client.on('connect', function() {
 });
 
 client.on('error', function(err) {
-  console.log("Error " + err);
+  console.log("Redis Error: " + err);
 });
 
 // // Uncomment this function to clear the database.
 // client.flushdb( function(err, succeeded) {
 //  console.log(succeeded);
 // });
-
-const uuidv4 = require('uuid/v4');
 
 const OrdersHashPrefix = "Orders:";
 const OrdersSetKey = "Orders:Ids";
@@ -46,12 +45,11 @@ app.get('/orders', GetOrders);
 var server = module.exports = app.listen(8080, function() {
   var host = server.address().address;
   var port = server.address().port;
-   
   console.log("App listening at http://%s:%s", host, port);
 });
 
 server.on('close', function(err) {
-  console.log("App closed!");
+  console.log("Server closed!");
 });
 
 /**
@@ -81,13 +79,14 @@ function PostOrder(req, res)
   console.log("Received POST with origin: " + origin + " and " +
     "destination: " + destination + "!\n");
 
-  if (isNaN(origin[0]) || isNaN(origin[1]) || isNaN(destination[0]) || isNaN(destination[1]))
-  {
+  if (isNaN(origin[0]) || isNaN(origin[1]) || 
+    isNaN(destination[0]) || isNaN(destination[1])) {
     HandleError(res, 500, "Body's origin or destination is invalid.");
     return;
   }
 
-  // TODO: Check that orderId doesn't already exist in the database.
+  // TODO: Even though it's unlikely, we should check that the generated 
+  //       orderId doesn't already exist in the database.
   var orderId = uuidv4();
   var status = "UNASSIGN";
 
@@ -106,9 +105,11 @@ function PostOrder(req, res)
     }
     else {
       return Promise.reject("Unable to get distance from Google Maps.");
-    } })
+    } 
+  })
 
   .then((distance) => {
+    // Store the order information in the database.
     var batch = client.batch();
     batch.HMSET(OrdersHashPrefix + orderId, 
       IdField, orderId,
@@ -117,13 +118,14 @@ function PostOrder(req, res)
     batch.SADD(OrdersSetKey, orderId);
     batch.EXEC();
 
+    // Send successful response back.
     res.status(200).send({
       [IdField]: orderId,
       [DistanceField]: distance,
-      [StatusField]: status }); })
+      [StatusField]: status }); 
+  })
 
-  .catch(error => 
-    HandleError(res, 500, error) );
+  .catch(error => HandleError(res, 500, error) );
 }
 
 /**
@@ -149,12 +151,13 @@ function PutOrderId(req, res)
   var orderId = req.params.id;
   console.log("Received PUT with id: " + orderId + "!\n");
 
-  if (req.body.status.toLowerCase() != StatusTaken)
-  {
-    var error = "Request body is invalid.";
-    return HandleError(res, 409, error, error);
+  // Validate the request body.
+  if (req.body.status.toLowerCase() != StatusTaken) {
+    HandleError(res, 409, "Request body is invalid.");
+    return;
   }
 
+  // Get the order information from the database.
   Promise.resolve()
   .then(() => {
     return new Promise((resolve, reject) => { 
@@ -182,14 +185,14 @@ function PutOrderId(req, res)
     }
     else {
       return Promise.reject("Unknown order status: " + status);
-    } })
+    } 
+  })
 
-  .catch(error => 
-    HandleError(res, 409, error) );
+  .catch(error => HandleError(res, 409, error) );
 }
 
 /**
- * 
+ * Handles the listing of orders based on the page number and limit number.
  * @param {Request} req The request query string contains:
  * {
  *   "page": <page_number>
@@ -208,18 +211,23 @@ function PutOrderId(req, res)
 function GetOrders(req, res)
 {
   var orders = [];
+
+  // Parse the query string. Note: A number with a decimal will drop the 
+  // numbers after the decimal.
   var page = parseInt(req.query.page);
   var limit = parseInt(req.query.limit);
-
   console.log(
     "Received GET with page: " + page + " and limit: " + limit + "!\n");
 
-  if (!Number.isInteger(page) || !Number.isInteger(limit) || page < 1 || limit < 1)
+  // Validate that page and limit are whole numbers.
+  if (!Number.isInteger(page) || !Number.isInteger(limit) || 
+    page < 1 || limit < 1) 
   {
     res.send(orders);
     return;
   }
 
+  // Get all the orders in the database.
   Promise.resolve()
   .then(() => {
     return new Promise((resolve, reject) => { 
@@ -234,10 +242,10 @@ function GetOrders(req, res)
     }); 
   })
 
+  // Get all the information for each order.
   .then((orderIds) => {
     var batch = client.batch();
-    for (var i = 0; i < orderIds.length; i++)
-    {
+    for (var i = 0; i < orderIds.length; i++) {
       batch.HGETALL(OrdersHashPrefix + orderIds[i]);
     }
 
@@ -253,11 +261,13 @@ function GetOrders(req, res)
     }); 
   })
   
-  .then((objects) => {
+  // Determine which orders to send back based on the page and limit.
+  // TODO: The orders retrieved from the database may not be sorted 
+  //       from oldest to newest.
+  .then(objects => {
     var startIndex = (page - 1) * limit;
     var counter = 0;
-    for (var i = startIndex; i < objects.length && counter < limit; i++)
-    {
+    for (var i = startIndex; i < objects.length && counter < limit; i++) {
       orders.push(objects[i]);
       counter++;
     }
@@ -268,9 +278,16 @@ function GetOrders(req, res)
 
   .catch(error => {
     console.log(error);
-    res.send(orders); });
+    res.send(orders); 
+  });
 }
 
+/**
+ * Responds an error back to the client.
+ * @param {Response} res 
+ * @param {integer} statusCode 
+ * @param {string} error 
+ */
 function HandleError(res, statusCode, error) {
   console.log("Error: " + error);
   res.status(statusCode).send( { "error": error } );
